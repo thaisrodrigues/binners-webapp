dcfd884649b33be06f43f19e41865c4e42a77575
Thais Rodrigues <thaisrodrigues25rj@gmail.com>
2016-05-27 21:09:30 -0300

    deleting old files

--- src/app/index.module.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('bProject', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'satellizer']);
-
-})();
-
-
